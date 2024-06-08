66aa481fc784992db7994dcc0d129caf794c4b61
sakshi <sakshikolekar2003@gmail.com>
2024-06-08 23:32:34 +0530

    #

---
+++ LetsWrite/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <>
+        <div>
+            <nav>
+             Home
+            </nav>
+        </div>
+        </>
+    );
+}
+export default Header;
